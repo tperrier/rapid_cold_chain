0d2d8eea52838285b258326e9961d26402066292
Trevor <tperrier@gmail.com>
2014-07-11 08:59:24 -0700

    added comments to parser and utils.Keywords

--- ccem_parser/parser/parser.py
+++ ccem_parser/parser/parser.py
@@ -2,6 +2,7 @@
 import abc,re,code
 from . import utils
 
+#Regular Expression of characters to be removed when cleaning messages
 MSG_REMOVE_CHARS = r'[\s]'
 
 class Parser:
@@ -9,21 +10,38 @@ class Parser:
 	remove_chars = re.compile(MSG_REMOVE_CHARS)
 	
 	def __init__(self,*args):
+		'''
+		Set the list of keywords based on keyword argument if present
+		or _auto_discovery() if not
+		'''
 		if len(args) == 0:
 			self.keywords = _auto_discovery()
 		else:
 			self.keywords = args
 	
 	def __call__(self,msg,pos=0):
+		'''
+		Make the parser object callable so my_parser() is equivalent to my_parser.parse()
+		''' 
 		self.parse(msg,pos)
 	
 	def parse(self,msg,pos=0):
+		'''
+		The main parsing function
+		'''
+		# Clean Message and create report object
 		msg = self.clean(msg)
 		msg_report = utils.ParseResult(msg)
 		while True:
+			#Attempt to parse message keywords at currrent possition 
+			#kw = returned keyword
+			#args = dict of args for keyword
+			#errors = list of errors found
+			#pos = new position = old position + length parsed string
 			kw,args,errors,pos = self._parse(msg,pos)
 			if kw is not None:
-				if kw.name != 'short':
+				if kw.name != 'short':  #the short kw is a special case and returns both an ft and sl.
+							#this can probably be made more elegant
 					msg_report.add(kw,args,errors)
 				else:
 					msg_report.add('ft',args[0],None)
@@ -35,11 +53,15 @@ class Parser:
 				break
 		if pos == 0: #no keyword found
 			msg_report.error('NO_KEYWORD_FOUND')
-		elif pos < len(msg):
+		elif pos < len(msg): # The are unparsed characters left on the message string
 			msg_report.error('INVALID_NEXT_KEYWORD')
 		return msg_report
 		
 	def _parse(self,s,pos):
+		'''
+		Loop through all keywords in parser and test if they match the current position
+		If a match is found use that keywords parse method at the current postion
+		'''
 		for kw in self.keywords:
 			 if kw.test(s,pos):
 				 args,errors,pos = kw.parse(s,pos)
@@ -51,6 +73,9 @@ class Parser:
 		return self.remove_chars.sub('',s)
 		
 def _auto_discovery():
+	'''
+	Attempts to load a keyword array from classes in keywords.KEYWORDS
+	'''
 	from keywords import KEYWORDS
 	return [kw() for kw in KEYWORDS]
 		
--- ccem_parser/parser/utils.py
+++ ccem_parser/parser/utils.py
@@ -1,6 +1,9 @@
 import re
 
 class Tokens:
+	'''
+	Regular expression tokens available for easy accesss
+	'''
 	integer = re.compile("\d+").match
 	whitespace = re.compile("\s+").match
 	singleletter = re.compile("[a-z]").match
@@ -9,15 +12,31 @@ class Tokens:
 
 	
 class Keyword(object):
-	
+	'''
+	All parser keywords should inherit from the utils.Keyword class	
+	'''
+
 	def __init__(self):
+		#If the subclass has not set a kw property set one based on the class name
 		if not hasattr(self,'kw'):
 			self.kw = self.__class__.__name__
+		#If the subclass has not set a name property set one based on the class name
 		if not hasattr(self,'name'):
-			self.name = self.kw
+			self.name = self.__class__.__name__
+		#create a regular expression object from the kw property
 		self.reg = re.compile(self.kw)
 		
 	def parse():
+		'''
+		Subclasses shouls implement a custom version of parse
+		parse(msg,pos=0)
+			msg: The message string to parse
+			pos: The possition in the string to start parsing
+		return:
+			args: dictionary of arguments found while parsing
+			erros: list of errors found while parsing
+			pos: the new position based on the end of parsing	
+		'''
 		pass
 		
 	def test(self,s,pos=0):

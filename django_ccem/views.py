935abd8df3525d894be115dd8e421f8468a4f566
Trevor <tperrier@gmail.com>
2014-07-15 17:58:13 -0700

    skeloton pagenator

--- django_ccem/views.py
+++ django_ccem/views.py
@@ -1,10 +1,28 @@
 # Create your views here.
-
-
 from django.shortcuts import render
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
+
+import models as ccem
 
 def base_view(request):
 	return render(request, 'ccem_sim/base.html')
 	
 def custom_view(request):
 	return render(request, 'custom.html')
+	
+def messages(request):
+	
+	message_list = ccem.Message.objects.all()
+	paginator = Paginator(message_list,10)
+	
+	page = request.GET.get('page',1)
+	try:
+		messages = paginator.page(page)
+	except PageNotAnInteger:
+		# If page is not an integer, deliver first page.
+		messages = paginator.page(1)
+	except EmptyPage:
+		# If page is out of range (e.g. 9999), deliver last page of results.
+		messages = paginator.page(paginator.num_pages)
+		
+	return render(request,'messages.html',{'messages':messages})

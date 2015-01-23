a5b9971399978485da3d6952faeff0e6f7dae052
Trevor <t.perrier@gmail.com>
2015-01-23 00:06:48 -0800

    Renamed wsgi

--- django_config/settings.py
+++ django_config/settings.py
@@ -68,6 +68,11 @@ MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'public', 'media')
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
 MEDIA_URL = '/media/'
 
+
+# URL prefix for static files.
+# Example: "http://media.lawrence.com/static/"
+STATIC_URL = '/static/'
+
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
@@ -76,10 +81,6 @@ if ON_OPENSHIFT:
     STATIC_ROOT = os.path.join(os.environ.get('OPENSHIFT_REPO_DIR'), 'wsgi', 'static')
 else:
     STATIC_ROOT = os.path.join(PROJECT_ROOT, STATIC_URL.strip("/"))
-
-# URL prefix for static files.
-# Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
 
 # Additional locations of static files to collect
 STATICFILES_DIRS = (
